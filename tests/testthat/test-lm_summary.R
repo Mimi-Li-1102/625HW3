b29353e9ceba35ab507d09bcc959cc85716d3f5b
mimi <mimili@umich.edu>
2023-11-15 17:31:54 -0500

    update test cases

--- tests/testthat/test-lm_summary.R
+++ tests/testthat/test-lm_summary.R
@@ -2,6 +2,17 @@ data(mtcars)
 lm <- lm(mpg ~ wt + hp + qsec, data = mtcars)
 sm <- summary(lm)
 test_summary <- lm_summary(lm)
-test_that("lm_summary_estimates", {
+
+# Test estimates in lm_summary()
+test_that("lm_summary_estimates1", {
   expect_equal(test_summary$Estimate[1], sm$coefficients[1, 1])
+})
+
+test_that("lm_summary_estimates2", {
+  expect_equal(test_summary$Estimate[2], sm$coefficients[2, 1])
+})
+
+# Test residual standard error
+test_that("residual_se", {
+  expect_equal(obtain_residual_info(lm)[1]$residual_se, sm$sigma)
 })
